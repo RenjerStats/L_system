1f914ad5434e28045efc94e452bcde5d9d7c1074
Павел Романьков <pavrom2006@gmail.com>
2024-04-06 15:52:31 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace L_system
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
