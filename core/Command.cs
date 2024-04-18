f56743edec50c7e74dadb46e914eaf0e889004ba
Павел Романьков <pavrom2006@gmail.com>
2024-04-18 18:07:24 +0300

    базовая форма приложения

---
+++ core/Command.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_system.core
+{
+    public enum CommandType
+    {
+        rotate = 'r',
+        move = 'm',
+        jump = 'j',
+        save = '[',
+        load = ']',
+        nothingDoing1 = '1',
+        nothingDoing2 = '2',
+        nothingDoing3 = '3'
+    }
+    public readonly struct Command(CommandType type, float val = 0)
+    {
+        public readonly float val = val;
+        public readonly CommandType type = type;
+
+        public override readonly bool Equals(object? obj)
+        {
+            if (obj is not Command)
+                return false;
+            else
+                return val == ((Command)obj).val && type == ((Command)obj).type;
+        }
+
+        public override readonly int GetHashCode()
+        {
+            return HashCode.Combine(val, (char)type);
+        }
+
+        public override readonly string? ToString()
+        {
+            return $"{type}: {val}";
+        }
+
+        public static bool operator ==(Command left, Command right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Command left, Command right)
+        {
+            return !(left == right);
+        }
+    }
+}
--- core/L_system_engine.cs
+++ core/L_system_engine.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace L_system.core
 {
-    internal class L_system_engine
+    public class L_system_engine(Command[] axiom, Dictionary<Command, Command[]> changeTo)
     {
+        private List<Command> axiom = new (axiom);
+        private readonly Dictionary<Command, Command[]> changeTo = changeTo;
+
+        public Command[] GetInstructions() => axiom.ToArray();
+
+        public void Iterate()
+        {
+            List<int> indexWhatAlreadyBeenUsed = [];
+
+            foreach (var change in changeTo.Keys)
+            {
+                var to = changeTo[change];
+                for (int i = 0; i < axiom.Count; i++)
+                {
+                    if (!indexWhatAlreadyBeenUsed.Contains(i) && change == axiom[i])
+                    {
+                            InsertingListWithReplacement(i, to);
+                        for (int q = 0; q < to.Length; q++)
+                            indexWhatAlreadyBeenUsed.Add(i + q);
+                    }
+                }
+            }
+        }
+
+        private void InsertingListWithReplacement(int index, Command[] to)
+        {
+            if (axiom.Count == 1)
+                axiom = new List<Command>(to);
+            else
+            {
+                List<Command> right = axiom[(index + 1)..^0];
+                axiom = axiom[0..index];
+
+                axiom.AddRange(to);
+                axiom.AddRange(right);
+            }
+        }
     }
 }
