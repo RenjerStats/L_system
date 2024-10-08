4f94e87e3bc3dde98d129ae7c5b89e4fe9841170
Павел Романьков <pavrom2006@gmail.com>
2024-10-08 10:27:01 +0300

    Разобрался в системе PropertyChanged

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using L_system.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace L_system
 {
@@ -22,6 +11,7 @@ namespace L_system
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowVM(this);
         }
     }
 }
---
+++ ViewModel/MainWindowVM.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace L_system.ViewModel
+{
+    public class MainWindowVM : BaseVM
+    {
+        #region Private
+        private int radiusBorderWindow;
+        private Window window;
+        #endregion
+
+        #region Public
+        public string Name { get; set; } = "Hello World";
+        #endregion
+
+        public MainWindowVM(Window window)
+        {
+            this.window = window;
+        }
+
+    }
+}
