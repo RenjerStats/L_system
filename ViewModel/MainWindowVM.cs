9d272405fb2be298b0123da4ff4431afdde5c2e1
Павел Романьков <pavrom2006@gmail.com>
2024-10-09 20:12:32 +0300

    Добавил тень для формочки

--- ViewModel/MainWindowVM.cs
+++ ViewModel/MainWindowVM.cs
@@ -7,10 +7,23 @@ namespace L_system.ViewModel
         #region Private
         private int radiusBorderWindow;
         private Window window;
+        private int shadowMarginSize = 10;
         #endregion
 
         #region Public
         public string Name { get; set; } = "Hello World";
+        public int ResizeBorder { get; set; } = 5;
+        public int HeadHeight { get; set; } = 50;
+
+
+        public Thickness ShadowMarginSize
+        {
+            get
+            { 
+                return new Thickness(window.WindowState == WindowState.Maximized ? 0 : shadowMarginSize);
+            }
+        }
+
         #endregion
 
         public MainWindowVM(Window window)
