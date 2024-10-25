7446a1983ca2bc007ed769e8f9881ad7a2c5975e
Павел Романьков <pavrom2006@gmail.com>
2024-10-25 14:42:55 +0300

    Тупик

--- View/UserControls/Header.xaml.cs
+++ View/UserControls/Header.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace L_system.View
+namespace L_system.View.UserControls
 {
     /// <summary>
     /// Логика взаимодействия для Header.xaml
